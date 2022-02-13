84bfcd7872958f60b5d1bef4a8a16c5103deedc3
Mads95 <santiagomadrid95@gmail.com>
2022-02-12 21:23:48 -0300

    commit homeworks resolves

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,18 +3,21 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+  return array[0];
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return array[array.length - 1];
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.length;
 }
 
 
@@ -23,6 +26,11 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  var newArray = [];
+  for (var i = 0; i < array.length; i++) {
+    newArray.push(array[i] + 1);
+  }
+  return newArray;
 }
 
 
@@ -30,6 +38,8 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push(elemento);
+  return array;
 }
 
 
@@ -38,6 +48,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift(elemento);
+  return array
 }
 
 
@@ -47,20 +59,39 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
-}
+  var frase = '';
+  for (var i=0; i <= palabras.length - 1 ; i++) {
+    if (i === palabras.length - 1) {
+    frase = frase + palabras[i];
+    } else {
+      frase = frase + palabras[i] + ' ';
+    }
+  }
+  return frase;
+} 
 
 
 function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
-}
-
+  for (var i = 0; i< array.length; i++){
+    if (elemento === array[i]){
+      return true;
+    } 
+    }
+  return false;
+  } 
 
 function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  var resultado = 0;
+  for (var i = 0; i < numeros.length; i++){
+    resultado = resultado + numeros[i];
+  }
+  return resultado;
 }
 
 
@@ -68,53 +99,101 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
-}
+  var suma = 0;
+  var NumeroDeAlumnos = 0;
+  for (var i = 0; i < resultadosTest.length; i++){
+    suma = suma + resultadosTest[i];
+    NumeroDeAlumnos= NumeroDeAlumnos + 1;
+  }
+  return promedio = suma/NumeroDeAlumnos;
+} 
 
 
 function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
-}
+  var MasGrande= numeros[0];
+  for (var i = 0; i < numeros.length - 1; i++) {
+    if (MasGrande < numeros[i+1]) {
+      MasGrande = numeros[i+1];
+    }
+  }
+  return MasGrande;
+} 
 
 
 function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
-}
+  if (arguments.length === 0) {
+    return 0
+  } else if (arguments.length === 1) {
+    return arguments[0];
+  } else {
+    var producto = arguments [0];
+    for (var i = 1; i < arguments.length; i++)
+    producto = producto * arguments[i];
+  }
+ return producto;
+} 
 
 
-function cuentoElementos(arreglo){
+function cuentoElementos(array){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  var contador = 0;
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] > 18) {
+      contador = contador + 1;
+    }
+  }
+  return contador;
 }
 
 
-function diaDeLaSemana(numeroDeDia) {
+function diaDeLaSemana() {
   //Suponga que los días de la semana se codifican como 1 = Domingo, 2 = Lunes y así sucesivamente. 
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+  if (arguments[0] < 8 && arguments[0] > 0){
+  if (arguments[0] === 1 || arguments[0] === 7) {
+    return 'Es fin de semana';
+  } else {
+    return 'Es dia Laboral';
+  }
+  } else {
+    return'Numero no admitido';
+  }
 } 
 
 
-function empiezaConNueve(n) {
+function empiezaConNueve() {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  var numero = arguments[0].toString();
+  if (numero.charAt(0) === '9') {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 
-function todosIguales(arreglo) {
+function todosIguales(array) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
-} 
+  for (var i = 0; i < array.length - 1; i++) {
+    if (array[i] !== array[i + 1]){
+      return false;
+    }
+  }
+  return true;
+}
 
 
 function mesesDelAño(array) {
@@ -122,6 +201,31 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  var NuevoArray=[];
+  var Enero= false;
+  var Marzo= false;
+  var Noviembre= false;
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === 'Enero') {
+      NuevoArray.push('Enero');
+      Enero = true;
+    } else if (i === array.length - 1 && Enero === false){
+      return 'No se encontraron los meses pedidos';
+    }
+    if (array[i] === 'Marzo') {
+      NuevoArray.push('Marzo');
+      Marzo = true;
+    } else if (i === array.length - 1 && Marzo === false){
+      return 'No se encontraron los meses pedidos';
+    }
+    if (array[i] === 'Noviembre') {
+      NuevoArray.push('Noviembre');
+      Noviembre = true;
+    } else if (i === array.length - 1 && Noviembre === false){
+      return 'No se encontraron los meses pedidos';
+    }
+  }
+  return NuevoArray;
 }
 
 
@@ -129,6 +233,13 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  var NuevoArray=[];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] > 100) {
+      NuevoArray.push(array[i]);
+    }
+  }
+  return NuevoArray;
 }
 
 
@@ -140,7 +251,20 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
-}
+  var array = [];
+  for (var i = 0; i < 10; i++) {
+    numero = numero + 2;
+    if (numero === i) {
+      break;
+    } 
+    array.push(numero);
+  }
+  if (numero === i) {
+    return 'Se interrumpió la ejecución';
+  } else {
+  return array;
+  }
+} 
 
 
 function continueStatement(numero) {
@@ -150,7 +274,17 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
-}
+  var array = [];
+  for (var i = 0; i < 10; i++) {
+    if (i === 5){
+      continue;
+    } else {
+    numero = numero + 2;
+    array.push(numero);
+    }
+    }
+  return array;
+} 
 
 
 // No modificar nada debajo de esta línea
---
+++ Resumen Prep-Course.js
@@ -0,0 +1,106 @@
+// Anatomía de una función:
+function miFuncion(/*Argumentos (cada argumento se separa con , (coma)*/) {
+    return (/*Declaración que permite extraer información desde dentro de la función cuando se invoque*/)
+}
+    // Invocación de una función:
+    miFuncion(/*Argumentos definidos*/);
+
+'-----------------------------------------------------------------------------------------------------------------------------------------'
+// Anatomía operador if: 
+if (/*Condiciónes*/) {
+
+} else if (/*Condiciónes*/){
+
+} else {
+
+}
+
+'-----------------------------------------------------------------------------------------------------------------------------------------'
+// Anatomía bucle for: 
+for (/*Se introducen declaraciones: 1. Declaramos una variable; 2. Expresión condicional; 3. Inclemento variable*/) {
+    '1. Se define la variable de iteración'; 
+    '2. Se determina el limite de iteración'; 
+    '3. Se define la forma en varia luego de cada ciclo la variable definida'
+}
+
+'-----------------------------------------------------------------------------------------------------------------------------------------'
+// Anatomía array (variables contenedoras de datos): 
+var array = ['Dato 0', 'Dato 1', 'etc.'];
+            // Los datos se separan con , (coma). Y están numerados según su orden en la matriz (empezando de 0).
+    // Métodos de array:
+        // 1_ .forEach: bucle que recorre cada elemento de un array
+            array.forEach(function(elemento, indice)){ // La función anonima entre parentesis es un callback
+                                                       // El segundo argumento ,"indice", es un elemento opcional.
+            }
+        // 2_ .reduce: recorre cada elemento de un array y los agrupa reduciendolos a un solo elemento
+        array.reduce(function(acc, elemento), acumuladorDeArranque){ // El argumento "acc" funciona como acumulador de los elementos ya recorridos
+          return // Siempre debe estar                               // El acumulador de arranque es opcional (empieza desde este elemento, si existe)
+        } // Devuelve acc
+        // 2_ .map: de igual funcionamiento y estructura que .forEach, salvo que devuelve un array
+        array.map(function(elemento, indice)){
+            // El segundo argumento ,"indice", es un elemento opcional.
+        } // Devuelve un array;
+'-----------------------------------------------------------------------------------------------------------------------------------------'
+// Anatomía objetos (variables contenedoras de datos): 
+var objeto = {
+            clave1 : valor1, 
+            cleve2 : valor2, 
+            claveN : valorN
+            }
+            // Los datos se separan con , (coma), excepto el último valor agregado.
+    // Acceder a datos de objetos (se pueden utilizar cualquera de las tre opciones siguientes):
+    objeto.claveN;
+    objeto['claveN'];
+    var clave = 'claveN';
+    objeto[clave];
+    // Modificar a datos de objetos (se pueden utilizar cualquera de las tre opciones siguientes):
+    objeto.claveN = otroValorN;
+    objeto['claveN'] = otroValorN;
+    var clave = 'claveN';
+    objeto[clave] = otroValorN;
+    // Eliminar clave:
+    delete objeto.claveN;
+    // Bucles for con objetos:
+    for (clave in objeto){
+        'Para cada clave en (in, palabra clave) el objeto'
+    }
+
+// Anatomía Métodos (funciones guardadas dentro de objetos):
+var objeto = {
+    metodo: function(){
+
+    }
+}
+                                                        //VER this (JSIV)
+'-----------------------------------------------------------------------------------------------------------------------------------------'
+// Anatomía clases (por convención se nombran en mayúscula): 
+    // 1º Anatomía: Función constructora
+    function Clase (valor){ // Cada uno de los "valores" se denominan "objetos de clase"??
+        this.clave = valor, // Se pueden separar las distintas thi.clave con ';' 
+                            // pero es mas intuitivo mantener la notación de arrays y objetos (separar elementos con ',').
+    }
+    // 2º Anatomía : Clase (de ES6)
+    class Clase{
+        constructor (valor) {
+        this.clave = valor, //"this." es reemplazado por "valor." cuando la clase es instanciada.
+        }
+    }   
+
+    // Instancia:
+    var valorInstanciado = new Clase(//Se ingresa el parametro que reemplaza a "".this");
+
+    // prototype: Creo que permite establecer un mecanismo a través del cual métodos credor heredan propiedades de objetos de clase.
+    Clase.prototype.prototipo = function(){
+        // Se establece la funcionalidad del método heredando objetos de clase a través de "this.clave".
+    }
+        // Invocación:
+        var invocación = new Clase(valor);
+        invocación.prototipo(); // Se ejecuta el metodo de prototype.
+'-----------------------------------------------------------------------------------------------------------------------------------------'
+
+// Anatomía Callback (funciones que poseen a otra función como argumento): 
+    // 1º Anatomía: Función constructora
+    function callback(argumento, cb){ // "argumento" es el argumento de la función llamada. "cb" variable usada como callback
+       return cb(argumento); 
+    }
+    '-----------------------------------------------------------------------------------------------------------------------------------------'
