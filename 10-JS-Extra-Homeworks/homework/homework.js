684f64c5044ea4a7aeb391642ac7e4f15b547884
Mads95 <santiagomadrid95@gmail.com>
2022-02-17 07:59:30 -0300

    progress on extra homework

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,11 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  var matriz =[];
+  for(var clave in objeto) {
+    matriz.push([clave, objeto[clave]]);
+  }
+  return matriz;
 }
 
 
@@ -18,6 +23,15 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  var objeto = {};
+  for(var i = 0; i < string.length; i++) {
+    if (objeto.hasOwnProperty(string[i])){
+      objeto[string[i]]++;
+    } else {
+      objeto[string[i]] = 1;
+    }
+  }
+  return objeto;
 }
 
 
@@ -26,6 +40,18 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  var capInFront = '';
+  for(var i = 0; i < s.length; i++) {
+    if (s.charAt(i) === s.charAt(i).toUpperCase()){
+      capInFront= capInFront + s[i];
+    }
+  }
+  for(var i = 0; i < s.length; i++) {
+    if (s.charAt(i) !== s.charAt(i).toUpperCase()){
+      capInFront= capInFront + s[i];
+    }
+  }
+  return capInFront;
 }
 
 
@@ -35,6 +61,25 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  var strArray = str.split(' ')
+  var invertidaArray= [];
+  var strinvertida = '';
+  for (let i = 0; i < strArray.length; i++) {
+    for (let j = strArray[i].length - 1; j >= 0; j--) {
+      strinvertida= strinvertida + strArray[i].charAt(j);
+    }
+    invertidaArray.push(strinvertida);
+    strinvertida = '';
+  }
+  var fraseInvertida = '';
+  for (let i = 0; i < invertidaArray.length; i++) {
+    if (invertidaArray[i] !== invertidaArray[invertidaArray.length -1]) {
+    fraseInvertida = fraseInvertida + invertidaArray[i] + ' ';
+    } else {
+      fraseInvertida = fraseInvertida + invertidaArray[i];
+    }
+  }
+  return fraseInvertida;
 } 
 
 
@@ -43,6 +88,16 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  var mirror = '';
+  var numeroString = numero.toString();
+  for (var i = numeroString.length -1 ; i >= 0; i--){
+    mirror = mirror + numeroString[i];
+  }
+  if (mirror === numeroString){
+  return 'Es capicua';
+  } else {
+    return 'No es capicua'
+  }
 }
 
 
@@ -50,6 +105,12 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  var sinABC= '';
+  for (var i = 0; i < cadena.length; i++)
+  if (cadena[i] !== 'a' && cadena[i] !== 'b' && cadena[i] !== 'c'){
+    sinABC = sinABC + cadena[i]
+  }
+  return sinABC;
 }
 
 
@@ -57,8 +118,28 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
-}
+  var arrOrd = [];
+  var lengthArr = arr.length;
+  for (let i = 0; i < lengthArr; i++) {
+    var smallerStr = arr[0];
+    var restArray = [];
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j].length < smallerStr.length){
+      smallerStr = arr[j];
+      }
+    }
+    arrOrd.push(smallerStr);
+    for (let k = 0; k < arr.length; k++) {
+      if (arr[k] !== smallerStr){
+        restArray.push(arr[k]);
+      }
+    }
+    arr = restArray;
+  } 
+  return arrOrd; 
+} sortArray(["You", "are", "beautiful", "looking"]); 
 
+ 
 
 function buscoInterseccion(arreglo1, arreglo2){
   //Existen dos arrays, cada uno con 5 números. A partir de ello, escribir una función que permita 
@@ -83,4 +164,3 @@ module.exports = {
    sortArray,
    buscoInterseccion,
 };
-
